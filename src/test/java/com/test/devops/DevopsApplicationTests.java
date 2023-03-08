6f83ac815b1f11be31d2f25c053c6a422175ec27
boris-gael <ndongboris92@gmail.com>
2023-03-08 15:34:39 +0100

    update assertion in test class

--- src/test/java/com/test/devops/DevopsApplicationTests.java
+++ src/test/java/com/test/devops/DevopsApplicationTests.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class DevopsApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.assertEquals("2", "2");
+		assertThat("2").isInstanceOf(String.class);
 	}
 
 }
