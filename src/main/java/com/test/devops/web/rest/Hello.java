e51b8700ef58d486dc4e1cd09060526ccc2f9078
boris-gael <ndongboris92@gmail.com>
2023-03-10 20:18:56 +0100

    add another api

--- src/main/java/com/test/devops/web/rest/Hello.java
+++ src/main/java/com/test/devops/web/rest/Hello.java
@@ -3,6 +3,7 @@ package com.test.devops.web.rest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,11 @@ public class Hello {
     @GetMapping("")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Hello from devops test app");
+    }
+
+    @GetMapping("/someone")
+    public ResponseEntity<String> helloSomeone(@RequestParam String name) {
+        return ResponseEntity.ok("Hello " + name + ", how are you?");
     }
 
 }
